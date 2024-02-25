35ce28eb51b8419b01730215f5af8a64a3c10659
Saurabh-Taylor <tailorsaurabh12@gmail.com>
2024-02-25 21:53:59 +0530

    added sticky scroll reveal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 import HeroSection from "@/components/HeroSection";
 import FeaturedCard from "@/components/FeaturedCard";
+import { WhyChooseUs } from "@/components/WhyChooseUs";
 
 export default function Home() {
  return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]" >
        <HeroSection/>
        <FeaturedCard/>
+       <WhyChooseUs/>
    </main>
   )
 }
